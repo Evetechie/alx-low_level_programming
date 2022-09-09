f0c22d6f40330ade522ba42f43f235da9ccd2a8d
Evetechie <muemaevelyn@gmail.com>
2022-09-09 06:48:11 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,10 @@
  * Description: using the main function
  * This program prints "Programming is like building a multilingual puzzle
  * Return: 0
- */
+*/
+
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
