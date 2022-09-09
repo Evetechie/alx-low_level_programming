b05dd1775cfc8573a2722d3685e44daca2ab94a1
Evetechie <muemaevelyn@gmail.com>
2022-09-09 08:04:35 -0700

    finito

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -6,7 +7,7 @@
  * Return: Always 1 (Success)
 */
 
-int main (void)
+int main(void)
 { 
 	write(2,
 			"and that piece of art is useful\"-Dora Korpar, 2015-10				-19\n",59);
