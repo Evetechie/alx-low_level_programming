f99a9595b42c42d60f307aa5f0035f8d97546beb
Evetechie <muemaevelyn@gmail.com>
2022-09-14 13:10:28 -0700

    Task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,6 +4,8 @@
   * @num1: The first integer to be added.
   * @num2: The second integer to be added
   *
+  * Description: addding two integers.
+  *
   * Return: The result of the addition.
   */
 int add(int num1, int num2)
