9f8862bdb215c962cfd83101de2aeaad0a4ebc8f
Evetechie <muemaevelyn@gmail.com>
2022-09-16 23:24:51 -0700

    Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,28 +9,26 @@
 
 void print_triangle(int size)
 {
-	int inc1, inc2;
 
-	if (size > 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
+	}
+	else
+	{
+	int i, j;
 
-		for (inc1 = 1; inc1 <= size; inc1++)
-		{
-			for ((inc2 = size - inc1); inc2 > 0; inc2--)
-			{
-				putchar(' ');
-			}
-
-			for (inc2 = 0; inc2 < inc1; inc2++)
-			{
-				putchar('#');
-			}
-			if (inc1 == size)
-			{
-				continue;
-			}
-			putchar('\n');
-		}
+	for (i = 1; i <= size; i++)
+	{
+	for (j = 1; j <= size; j++)
+	{
+	_putchar(' ');
+	}
+	for (j = 1; j <= i; j++)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
+	}
 	}
 }
