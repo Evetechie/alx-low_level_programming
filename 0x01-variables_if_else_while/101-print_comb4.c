7a5fce48b57b40d789be5b9adc8211a85b615cd6
Evetechie <muemaevelyn@gmail.com>
2022-09-19 14:46:03 -0700

    Task 8 redo

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,26 +16,27 @@ int main(void)
 		b = a + 1;
 		while (b <= '8')
 		{
-			c = b + 1;
-			while (c <= '9')
-			{
-				putchar(a);
-				putchar(b);
-				putchar(c);
-				if (a == '7' && b == '8' && c == '9')
-				{
-					putchar('\n');
-				}
-				else
-				{
-					putchar(',');
-					putchar(' ');
-				}
-				c++;
-			}
+		c = b + 1;
+		while (c <= '9')
+		{
+			putchar(a);
+			putchar(b);
+			putchar(c);
+		if (a == '7' && b == '8' && c == '9')
+		{
+			putchar('\n');
+		}
+			
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+			c++;
+		}
 			b++;
 		}
-		c++;
+			c++;
 	}
 	return (0);
 }
