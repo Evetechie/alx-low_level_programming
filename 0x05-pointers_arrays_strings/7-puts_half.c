c125298ee4e760e498d6ffb096ad3467f9e44a9e
Evetechie <muemaevelyn@gmail.com>
2022-09-19 13:20:26 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,21 +9,20 @@
 
 void puts_half(char *str)
 {
-	int index, half;
+	int len = 0;
 
-	index = 0;
-	while (str[index] != '\0')
-		index++;
-
-	half = index / 2;
-
-	if (index % 2 == 1)
-		half++;
-
-	while (half < index)
+	while (*str != '\0')
 	{
-		putchar(str[half]);
-		half++;
+		len++;
+		str++;
 	}
+
+	str -= (len / 2);
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+
 	putchar('\n');
 }
