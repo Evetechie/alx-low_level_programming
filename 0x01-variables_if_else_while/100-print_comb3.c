e601c4036af30f97408a30195eaa83f98604d890
Evetechie <muemaevelyn@gmail.com>
2022-09-20 06:38:54 -0700

    Task 10 last redo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 
 /**
- * Main - print single digit numbers starting from 0
+ * main - print single digit numbers starting from 0
  *
  * Return: returns zero at the end
  */
 
 int main(void)
 {
-	int i, j;
 
-	for (i = 0; i <= 9; i++)
+int i, j;
+
+for (i = 0; i <= 9; i++)
+{
+	for (j = 1; j <= 9; j++)
 	{
-		for (j = 1; j <= 9; j++)
+		if (j > i)
 		{
-			if (j > i)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-				if (i != 8)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar(i + '0');
+			putchar(j + '0');
+		}
+		if (i != 8)
+		{
+			putchar(',');
+			putchar(' ');
 		}
 	}
+}
+
 	putchar('\n');
 
 	return (0);
