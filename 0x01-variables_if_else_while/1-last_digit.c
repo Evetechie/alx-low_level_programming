a80578cb44f9c3a0f69539231a75de48c4a65fa7
Evetechie <muemaevelyn@gmail.com>
2022-09-20 07:41:15 -0700

    Task 1 last redo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,19 +3,18 @@
 #include <stdio.h>
 
 /**
- * main - print random number to variable
+ * main - prints random number to variable
  *
- * Return: always successful
+ * Return: Always (Successful)
  */
 int main(void)
 {
-
 	int n;
 	char last[] = "Last digit of";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("%s%d is %d and is", last, n, n % 10);
+	printf("%s %d is %d and is", last, n, n % 10);
 	if (n % 10 > 5)
 	{
 		printf("greater than 5\n");
