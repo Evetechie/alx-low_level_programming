3d379c00a3f329b0badc8d6bd5e1821b011ceb81
Evetechie <muemaevelyn@gmail.com>
2022-09-20 07:23:27 -0700

    Task 10 last redo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,24 +9,24 @@
 int main(void)
 {
 
-int i, j;
+	int i, j;
 
-for (i = 0; i <= 9; i++)
-{
-	for (j = 1; j <= 9; j++)
+	for (i = 0; i <= 9; i++)
 	{
-		if (j > i)
+		for (j = 1; j <= 9; j++)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-		}
-		if (i != 8)
-		{
-			putchar(',');
-			putchar(' ');
+			if (j > i)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
-}
 
 	putchar('\n');
 
