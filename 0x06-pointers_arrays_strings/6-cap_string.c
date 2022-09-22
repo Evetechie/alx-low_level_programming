dea20236db52ea5e0e7dad1a3d2f18cb8dfe943f
Evetechie <muemaevelyn@gmail.com>
2022-09-22 05:28:45 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: returns lengthas integer;
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
+/**
+ * cap_string - function that capitalizes first character of a word
+ * @str: string to capitalize
+ * Return: returns capitalized string
+ */
+
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[++index])
+	{
+		while (!(str[index] >= 'a') && (str[index] <= 'z'))
+		       index++;
+		if (str[index - 1] == ' ' ||
+			str[index - 1] == '\t' ||
+			str[index - 1] == '\n' ||
+			str[index - 1] == ',' ||
+			str[index - 1] == ';' ||
+			str[index - 1] == '.' ||
+			str[index - 1] == '!' ||
+			str[index - 1] == '?' ||
+			str[index - 1] == '"' ||
+			str[index - 1] == '(' ||
+			str[index - 1] == ')' ||
+			str[index - 1] == '{' ||
+			str[index - 1] == '}')
+			str[index] -= 32;
+	}
+
+	return (str);
+}
