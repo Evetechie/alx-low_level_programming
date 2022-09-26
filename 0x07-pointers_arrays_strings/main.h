824bfcc7923fe4180c0f364eead35b2deee1359e
Evetechie <muemaevelyn@gmail.com>
2022-09-26 11:44:58 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,12 +3,12 @@
 #include <stddef.h>
 #include <stdio.h>
 /**
- * void prototypes()
- * int prototypes()
- * char prototypes()
+ * void prototypes(void)
+ * int prototypes(int)
+ * char prototypes(char)
  */
-int _putchar(char);
-char *_memset(char *s, char b, unsigned int n;)
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
