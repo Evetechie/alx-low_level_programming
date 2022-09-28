b15e70ceec698fe2927085ca7b817bab4c8f47a6
Evetechie <muemaevelyn@gmail.com>
2022-09-28 09:04:32 -0700

    100-is_palindrome.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -17,6 +17,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+void check(char *s, int start, int end, int *flag);
 int wildcap(char *s1, char *s2);
 
 #endif /*MAIN_H*/
