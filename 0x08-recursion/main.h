02d7c80ddd01339c67e4e26b7d0a52bc2889dc20
Evetechie <muemaevelyn@gmail.com>
2022-09-28 09:10:47 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -16,6 +16,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int _sqrt(int n, int x);
 int is_palindrome(char *s);
 void check(char *s, int start, int end, int *flag);
 int wildcap(char *s1, char *s2);
