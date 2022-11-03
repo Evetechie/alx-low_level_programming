894b9e0bc6dccea51096bffa0f4737d0d60be7c8
Evetechie <muemaevelyn@gmail.com>
2022-11-03 03:46:39 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,8 +18,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL);
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (len = 0; text_content[len]; len++)
+			;
 	}
 
 	o = open(filename, O_WRONLY | O_APPEND);
