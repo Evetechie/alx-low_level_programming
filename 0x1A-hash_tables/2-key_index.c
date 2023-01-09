3810ca526ff6f023e516d9d443abde0860edfa4b
Evetechie <muemaevelyn@gmail.com>
2023-01-09 10:06:14 -0800

    hash tables

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,7 +2,7 @@
 
 /**
  * key_index - Get the index at which a key/value
- * 	pair should be stored in array of a hash table.
+ * pair should be stored in array of a hash table.
  * @key: The key to get the index of.
  * @size: The size of the array of the hash table.
  *
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -7,7 +7,7 @@
  * @value: The value associated with key.
  *
  * Return: Upon failure - 0.
- * 	Otherwise - 1.
+ * Otherwise - 1.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
